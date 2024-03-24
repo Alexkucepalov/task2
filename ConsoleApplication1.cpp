6ff41693fe641eff73d2a8abf1cbae5362b771fb
Alex <kucepalov134@mail.ru>
2024-03-24 12:16:49 +0300

    add all file

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int totalChest = 80;
+    int count = 0;
+
+    int left = 1;
+    int right = totalChest;
+
+    while (left <= right)
+    {
+        int middle = (left + right) / 2;
+
+        int goldenChest = rand() % totalChest + 1;
+        if (goldenChest <= middle)
+        {
+            right = middle;
+        }
+        else
+        {
+            left = middle + 1;
+        }
+        count++;
+    }
+    
+    std::cout << "Мы нашли золотую хвостовку в студуке под номером " << left << std::endl;
+    std::cout << "Минимальное кол-во испытаний оказалось " << count << std::endl;
+    return 0;
+}
